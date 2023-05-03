70ef22ec21bc65c0720c39362b9dacc195443488
Eliud de León <eliud.deleon.10@gmail.com>
2023-05-03 00:44:58 -0600

    Started working on main_parser module

--- src/parser/main_parser.rs
+++ src/parser/main_parser.rs
@@ -1,5 +1,7 @@
 use crate::{
-    ast::{Block, Destructure, Stmt, VarDecl},
+    ast::{
+        Block, Destructure, PropertyDestructure, PropertyName, Stmt, VarDecl, VarDeclAssignment,
+    },
     lexer::Token,
     recursive_parser,
 };
@@ -17,7 +19,12 @@ use std::{
     vec::IntoIter,
 };
 
-use super::primitive_parser::id_parser;
+use super::{
+    expr_parser::{expr_parser, EXPR_PARSER},
+    primitive_parser::{
+        id_parser, scope_specifier_parser, string_parser, type_specifier_parser, var_type_parser,
+    },
+};
 
 pub type ParserError<'a, T> = extra::Err<Rich<'a, T>>;
 pub type ParserInput<'a> =
@@ -32,23 +39,99 @@ pub fn stmt_end_parser<'i>(
     just(Token::StmtEnd).ignored()
 }
 
-pub fn var_decl_parser<'i>(
-) -> impl Parser<'i, ParserInput<'i>, VarDecl<'i>, ParserError<'i, Token<'i>>> + Clone {
-    todo()
-}
-
 recursive_parser!(
     DESTRUCTURE_PARSER,
     destructure_parser,
     Destructure<'static>,
-    declarations {},
+    declarations {
+        let destructure = DESTRUCTURE_PARSER.read().unwrap().clone()
+    },
     main_definition {
         let id = id_parser().map(|id| Destructure::Id(id));
+        let arr = destructure
+            .clone()
+            .separated_by(just(Token::Comma))
+            .collect::<Vec<_>>()
+            .delimited_by(just(Token::LSqBracket), just(Token::RSqBracket))
+            .map(Destructure::Array);
+        let obj = {
+            let str_prop_name = string_parser()
+                .map(|s| s.to_string())
+                .map(PropertyName::String);
+            let id_prop_name = id_parser().map(PropertyName::Id);
 
-        id
+            let alias = just(Token::Colon).ignore_then(destructure.clone());
+
+            let str_prop = str_prop_name
+                .then(alias.clone())
+                .map(|(name, alias)| PropertyDestructure {
+                    name,
+                    alias: Some(alias),
+                });
+            let id_prop = id_prop_name
+                .then(alias.or_not())
+                .map(|(name, alias)| PropertyDestructure { name, alias });
+
+            str_prop.or(id_prop)
+        };
+        let obj = obj
+            .separated_by(just(Token::Comma))
+            .collect::<Vec<_>>()
+            .delimited_by(just(Token::LBracket), just(Token::RBracket))
+            .map(Destructure::Object);
+
+        id.or(arr).or(obj)
     },
-    definitions {}
+    definitions {
+        if destructure.is_defined() {
+            destructure_parser();
+        }
+    }
 );
+
+pub fn var_decl_assignment_parser<'i: 'static>(
+) -> impl Parser<'i, ParserInput<'i>, VarDeclAssignment<'i>, ParserError<'i, Token<'i>>> + Clone {
+    // Declarations
+    let destructure = DESTRUCTURE_PARSER.read().unwrap().clone();
+    let expr = EXPR_PARSER.read().unwrap().clone();
+
+    // Main definition
+    let var_decl_as = destructure
+        .clone()
+        .then(type_specifier_parser().or_not())
+        .then_ignore(just(Token::AssignmentEq))
+        .then(expr.clone())
+        .map(|((destructure, var_type), expr)| VarDeclAssignment {
+            destructure,
+            var_type,
+            expr,
+        });
+
+    // Definitions
+    if destructure.is_defined() {
+        destructure_parser();
+    }
+    if expr.is_defined() {
+        expr_parser();
+    }
+
+    var_decl_as
+}
+
+pub fn var_decl_parser<'i: 'static>(
+) -> impl Parser<'i, ParserInput<'i>, VarDecl<'i>, ParserError<'i, Token<'i>>> + Clone {
+    let decl_assignments_p = var_decl_assignment_parser()
+        .separated_by(just(Token::Comma))
+        .at_least(1)
+        .collect::<Vec<_>>();
+
+    scope_specifier_parser()
+        .then(decl_assignments_p)
+        .map(|(scope_spec, decl_assignments)| VarDecl {
+            scope_spec,
+            decl_assignments,
+        })
+}
 
 recursive_parser!(
     STMT_PARSER,
