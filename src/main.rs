a0ef09da0165ede31fc990f312b5f9f919dcdfbc
Eliud de León <eliud.deleon.10@gmail.com>
2023-05-20 18:41:32 -0600

    Changed compile input from string literal to file

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,17 @@ mod lexer;
 mod parser;
 
 use compiler::main_codegen::compile_yep;
+use std::{fs, path::Path};
 
 fn main() {
-    let input = r#"
-    extern i32 printf(*i8, ...);
+    let tests_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/tests");
+    let tests_dir_path = Path::new(tests_dir);
 
-    let x: u8[5] = [1, 2, 3, 4, 5];
-    for(let i: u32 = 0; i < 5; i += 1) {
-        printf("%dth element is %d\n", i, x[i]);
-    }
-    "#;
+    let input = fs::read_to_string(tests_dir_path.join("test.yep")).unwrap();
+    let input_ref: &'static str = Box::leak(input.into_boxed_str());
 
-    let project_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/compiled");
-    let res = compile_yep(input, project_dir, "test");
+    let compiled_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/compiled");
+    let res = compile_yep(input_ref, compiled_dir, "test");
     match res {
         Ok(_) => {}
         Err(err) => eprintln!("{}", err),
